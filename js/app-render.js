2037fc5b483bb5b03c636baa3831043e436449df
cha0smagick <alekos20@gmail.com>
2026-06-24 18:55:09 -0500

    va 2

--- js/app-render.js
+++ js/app-render.js
@@ -391,7 +391,7 @@ function renderAppDetails() {
         loadHotmartWidget();
     } else if (item.url) {
         const playIconBtn = '<svg class="play-icon" viewBox="0 0 24 24" fill="currentColor" style="width:22px;height:22px;"><path d="M3.609 1.814L13.792 12 3.61 22.186a.996.996 0 0 1-.61-.92V2.734a1 1 0 0 1 .609-.92zm10.89 10.893l2.302 2.302-10.937 6.333 8.635-8.635zm3.199-3.199l2.807 1.626a1 1 0 0 1 0 1.732l-2.807 1.626L15.206 12l2.492-2.492zM5.864 2.658L16.8 8.99l-2.302 2.302-8.634-8.634z"/></svg>';
-        actionButton = `<a href="${item.url}" class="play-store-btn pulse" target="_blank">${playIconBtn} GET IT ON Google Play</a>`;
+        actionButton = `<a href="${item.url}" class="play-store-btn pulse" target="_blank">${playIconBtn} GET IT ON PLAY STORE</a>`;
     } else {
         actionButton = '<button class="cta-button disabled">Coming Soon</button>';
     }
